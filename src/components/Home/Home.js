72a75e0d6e7e347a2d4e35f83ef71ec1221b6a1a
dvlprJobayer <nesarahammed019@gmail.com>
2022-05-01 14:28:23 +0600

    social login component done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
         <div>
             <Banner />
             <div className="container">
-                <h2 className='color text-center display-4 fw-normal my-4'>Inventory items</h2>
+                <h2 className='color text-center display-4 my-4'>Inventory items</h2>
                 <Row xs={1} md={2} lg={3} className="g-4 mb-5">
                     {
                         items.map(item => <SingleItem key={item._id} item={item} />)
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,19 +3,24 @@ import { Link } from 'react-router-dom';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
+
+    const handleLogin = e => {
+        e.preventDefault();
+    }
+
     return (
         <div className='container mt-3'>
             <div className="row">
                 <div className="col-md-6 pb-5 pt-4 border-end pe-5">
                     <h1 className='fw-normal text-center text-uppercase mb-3 color'>Log in</h1>
-                    <form>
+                    <form onSubmit={handleLogin}>
                         <div className="mb-3">
                             <label className='form-label fs-5' htmlFor="email">Email <span className='color'>*</span></label>
-                            <input className='form-control fs-5' id='email' type="email" name='email' />
+                            <input className='form-control fs-5' id='email' type="email" name='email' required />
                         </div>
                         <div className="mb-4">
                             <label className='form-label fs-5' htmlFor="password">Password <span className='color'>*</span></label>
-                            <input className='form-control fs-5' id='password' type="password" name='password' />
+                            <input className='form-control fs-5' id='password' type="password" name='password' required />
                         </div>
                         <input className='btn w-100 btn-lg btn-main text-uppercase' type="submit" value="Login" />
                     </form>
